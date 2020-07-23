687c92a6f59df53d1d2b3563796dcc1d344becfc
kaushal8707 <kaushal.singh029@gmail.com>
2020-07-23 11:46:40 +0530

    Initial commit

---
+++ src/test/java/com/always/learner/ClientConfig/ClientConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.always.learner.ClientConfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
